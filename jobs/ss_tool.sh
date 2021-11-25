1b0e82307be77a0dbe2d123eb2f84eda57d325be
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2021-11-25 17:17:02 +0000

    Update ss_tool.sh

--- jobs/ss_tool.sh
+++ jobs/ss_tool.sh
@@ -2,7 +2,7 @@
 set -e
 apt-get -qq update
 apt-get --yes install --no-install-recommends build-essential gcc git
-
+git pull
 gcc -s -fPIE -O3 -o usr/bin/ss-tool src/main.c
 sed -i "s/${ss_tool_old:?}/${ss_tool:?}/g" version/version
 git add usr/bin/ss-tool version/version
