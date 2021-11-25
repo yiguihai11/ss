995756ce854cb56bcafe04d34409e98309387a2e
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2021-11-25 16:35:14 +0000

    Update push.sh

--- jobs/push.sh
+++ jobs/push.sh
@@ -4,4 +4,5 @@ apt-get -qq update
 apt-get --yes install --no-install-recommends git
 git config --global user.email "$GITLAB_USER_EMAIL"
 git config --global user.name "$GITLAB_USER_LOGIN"
-git remote set-url origin ${CI_SERVER_PROTOCOL:=https}://${GITLAB_USER_LOGIN:?}:${my_access_token:?}@${CI_REPOSITORY_URL#*@}
+cru=${CI_REPOSITORY_URL#*@}
+git remote set-url origin ${CI_SERVER_PROTOCOL:=https}://${GITLAB_USER_LOGIN:?}:${my_access_token:?}@${cru:?}
