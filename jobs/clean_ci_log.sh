8a5490c6e05ddf6af212d3d909ce22059697523e
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2021-11-25 15:34:41 +0000

    Update clean_ci_log.sh

--- jobs/clean_ci_log.sh
+++ jobs/clean_ci_log.sh
@@ -7,6 +7,7 @@ apt-get --yes install --no-install-recommends curl ca-certificates jq
 for PIPELINE in $(curl --header "PRIVATE-TOKEN: ${my_access_token:?}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID:?}/pipelines?per_page=100&sort=asc" | jq '.[].id'); do
 	if [ "$PIPELINE" -ne ${CI_PIPELINE_ID:-0} ]; then
 		echo "Deleting pipeline $PIPELINE"
-		curl --header "PRIVATE-TOKEN: ${my_access_token:?}" --request "DELETE" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID:?}/pipelines/$PIPELINE"
+		
+		curl -L -s -q --header "PRIVATE-TOKEN: ${my_access_token:?}" --request "DELETE" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID:?}/pipelines/$PIPELINE"
 	fi
 done
