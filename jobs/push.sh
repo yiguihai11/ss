04e5de7a27c6f86bb013fd3b8f7432dc44371124
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2021-11-25 17:44:55 +0000

    Update push.sh

--- jobs/push.sh
+++ jobs/push.sh
@@ -4,7 +4,7 @@ apt-get -qq update
 apt-get -qq --yes install --no-install-recommends git ca-certificates
 
 https_url="${CI_SERVER_PROTOCOL:=https}://${GITLAB_USER_LOGIN:?}:${my_access_token:?}@${CI_REPOSITORY_URL#*@}"
-git pull --ff-only "$https_url"
+git pull --ff-only "$https_url" HEAD:${CI_COMMIT_REF_NAME:?}
 #git config --global http.sslVerify false
 git config --global user.email "$GITLAB_USER_EMAIL"
 git config --global user.name "$GITLAB_USER_LOGIN"
