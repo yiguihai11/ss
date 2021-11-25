9cf5251c9a21f1985a4b834d6196771379d0824a
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2021-11-25 15:32:12 +0000

    Update clean_ci_log.sh

--- jobs/clean_ci_log.sh
+++ jobs/clean_ci_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 apt-get -qq update
-apt-get --yes install --no-install-recommends curl
+apt-get --yes install --no-install-recommends curl ca-certificates jq
 
 #https://qa.1r1g.com/sf/ask/3734890491/
 for PIPELINE in $(curl --header "PRIVATE-TOKEN: ${my_access_token:?}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID:?}/pipelines?per_page=100&sort=asc" | jq '.[].id'); do
