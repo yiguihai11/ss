7fd4982f1b09fe669bde28562f707926c3618bbe
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2021-11-25 17:16:08 +0000

    Update ss_main.sh

--- jobs/ss_main.sh
+++ jobs/ss_main.sh
@@ -4,6 +4,7 @@ apt-get -qq update
 apt-get --yes install --no-install-recommends shellcheck gzip
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
 $(go env GOPATH)/bin/shfmt -version
+git pull
 $(go env GOPATH)/bin/shfmt -l -s -w src/manager.sh
 shellcheck --shell=bash src/manager.sh
 gzexe src/manager.sh
