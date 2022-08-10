26b07293a9b810ee29fcc68a42ece3b9bb78dd2e
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2022-08-10 15:01:19 +0000

    替换 ip-scanner.sh

--- jobs/ip-scanner.sh
+++ jobs/ip-scanner.sh
@@ -12,6 +12,31 @@ apt-get --yes install --no-install-recommends \
 git clone --depth 1 https://github.com/ip-scanner/cloudflare cloudflare
 cd cloudflare
 rm -rf .git
+Traffic() {
+	local i=${1%.*}
+	if [ "$i" -lt 1024 ]; then
+		printf '%d B' "$i"
+	elif [ "$i" -lt $((1024 ** 2)) ]; then
+		awk 'BEGIN{printf "%.2f KB",('"$i"' / 1024)}'
+	elif [ "$i" -lt $((1024 ** 3)) ]; then
+		awk 'BEGIN{printf "%.2f MB",('"$i"' / (1024 ^ 2))}'
+	elif [ "$i" -lt $((1024 ** 4)) ]; then
+		awk 'BEGIN{printf "%.2f GB",('"$i"' / (1024 ^ 3))}'
+	elif [ "$i" -lt $((1024 ** 5)) ]; then
+		awk 'BEGIN{printf "%.2f TB",('"$i"' / (1024 ^ 4))}'
+	elif [ "$i" -lt $((1024 ** 6)) ]; then
+		awk 'BEGIN{printf "%.2f PB",('"$i"' / (1024 ^ 5))}'
+	elif [ "$i" -lt $((1024 ** 7)) ]; then
+		awk 'BEGIN{printf "%.2f EB",('"$i"' / (1024 ^ 6))}'
+	elif [ "$i" -lt $((1024 ** 8)) ]; then
+		awk 'BEGIN{printf "%.2f ZB",('"$i"' / (1024 ^ 7))}'
+	elif [ "$i" -lt $((1024 ** 9)) ]; then
+		awk 'BEGIN{printf "%.2f YB",('"$i"' / (1024 ^ 8))}'
+	elif [ "$i" -lt $((1024 ** 10)) ]; then
+		awk 'BEGIN{printf "%.2f BB",('"$i"' / (1024 ^ 9))}'
+	fi
+}
+
 for i in *.txt; do
 	x=0
 	while IFS= read -r line || [ -n "$line" ]; do
@@ -19,13 +44,16 @@ for i in *.txt; do
 			break
 		fi
 		sslocal --local-addr 127.0.0.1:1081 --password "MzNiMzA4M2QtNjU5ZC00ZDY0LTg3YWUtNTA1M2JlNgo=" --encrypt-method 2022-blake3-chacha20-poly1305 --plugin v2ray-plugin --plugin-opts "path=f846917c6e48f0727a2e894fc5cc3440c3265415;host=mikutap.ml;tls" --server-addr ${line}:443 --daemonize --daemonize-pid test.pid
+		echo "$i"
 		sleep 1
-		ret=$(curl -4 -s -o /dev/null -w '%{response_code}' --connect-timeout 5 -x socks5://127.0.0.1:1081 https://wap.baidu.com)
-		if [ "${ret:-0}" -eq 200 ]; then
-			echo $i $line ✔
-			echo "$i $line" >>${CI_PROJECT_DIR}/ip.txt
-            echo "$line"|grep -oP '([0-9]+\.){3}[0-9]+?' >>${CI_PROJECT_DIR}/ip2.txt
-			cat >>${CI_PROJECT_DIR}/ss-local.json <<EOF
+		if [ "$(curl -4 -s -o /dev/null -w '%{response_code}' --connect-timeout 5 -x socks5://127.0.0.1:1081 https://wap.baidu.com)" -eq 200 ]; then
+			response=$(curl -4 -s -o /dev/null -w '%{response_code}|%{speed_download}' --connect-timeout 5 -x socks5://127.0.0.1:1081 https://mikutap.ml/300.png)
+			if [ ${response%|*} -eq 200 ]; then
+				speed_download=$(Traffic ${response#*|})
+				echo "$line ${speed_download:-0}/s"
+				echo "$i $line ${speed_download:-0}/s" >>${CI_PROJECT_DIR}/ip.txt
+				echo "$line" | grep -oP '([0-9]+\.){3}[0-9]+?' >>${CI_PROJECT_DIR}/ip2.txt
+				cat >>${CI_PROJECT_DIR}/ss-local.json <<EOF
 		{
             "disabled": false,
             "address": "$line",
@@ -51,6 +79,7 @@ for i in *.txt; do
             "udp_weight": 0.1,
         },
 EOF
+			fi
 		fi
 		kill $(cat test.pid) 2>/dev/null
 		rm -f test.pid
