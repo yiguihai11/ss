e1c8dfad2f53cb8ef349f80d6f43b7a05e2f1624
爱翻墙的红杏 <5127273-yiguihai@users.noreply.gitlab.com>
2022-08-12 15:28:15 +0000

    替换 sslocal.sh

--- jobs/sslocal.sh
+++ jobs/sslocal.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 source jobs/golang.sh
-if [[ "$PLATFORM" == "arm-"* ]]; then
-   go_arch='arm'
-   host_cc="$TOOLCHAIN/bin/armv7a-linux-androideabi${API}-clang"
-   host_cxx="$TOOLCHAIN/bin/armv7a-linux-androideabi${API}-clang++"
-elif [[ "$PLATFORM" == "aarch64-"* ]];then
-   go_arch='arm64'
-   host_cc="$TOOLCHAIN/bin/${PLATFORM}${API}-clang"
-   host_cxx="$TOOLCHAIN/bin/${PLATFORM}${API}-clang++"
-fi
-
 source jobs/ndk.sh
 ln -s ${NDK_PREFIX}/lib/${PLATFORM}/${API}/libc.a ${NDK_PREFIX}/lib/${PLATFORM}/${API}/libpthread.a
 ln -s ${NDK_PREFIX}/lib/${PLATFORM}/${API}/libc.so ${NDK_PREFIX}/lib/${PLATFORM}/${API}/libpthread.so
 bash jobs/rust.sh
+
+if [[ $PLATFORM == "arm-"* ]]; then
+	go_arch='arm'
+	host_cc="$TOOLCHAIN/bin/armv7a-linux-androideabi${API}-clang"
+	host_cxx="$TOOLCHAIN/bin/armv7a-linux-androideabi${API}-clang++"
+elif [[ $PLATFORM == "aarch64-"* ]]; then
+	go_arch='arm64'
+	host_cc="$TOOLCHAIN/bin/${PLATFORM}${API}-clang"
+	host_cxx="$TOOLCHAIN/bin/${PLATFORM}${API}-clang++"
+fi
 
 apt-get --yes install --no-install-recommends zip xz-utils wget
 wget https://nodejs.org/dist/v16.16.0/node-v${NODEJS_VER}-linux-x64.tar.xz
@@ -34,29 +34,28 @@ zip -r - assets/build >assets.zip
 # 获得当前版本号、Commit
 export COMMIT_SHA=$(git rev-parse --short HEAD)
 export VERSION=$(git describe --tags)
-env AR=$TOOLCHAIN/bin/llvm-ar CC=${PLATFORM}${API}-clang CXX=${PLATFORM}${API}-clang++ LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -a -o cloudreve -ldflags "-s -w -X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.BackendVersion=$VERSION' -X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.LastCommit=$COMMIT_SHA'"
+env AR=$TOOLCHAIN/bin/llvm-ar CC=$host_cc CXX=$host_cxx LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -a -o cloudreve -ldflags "-s -w -X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.BackendVersion=$VERSION' -X 'github.com/cloudreve/Cloudreve/v3/pkg/conf.LastCommit=$COMMIT_SHA'"
 ls
 cd ..
 
 git clone --depth 1 https://github.com/teddysun/v2ray-plugin.git
 cd v2ray-plugin
 go get -d ./...
-env AR=$TOOLCHAIN/bin/llvm-ar CC=${PLATFORM}${API}-clang CXX=${PLATFORM}${API}-clang++ LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -ldflags "-X main.VERSION=$(date -u +%Y%m%d) -s -w" -o v2ray-plugin
+env AR=$TOOLCHAIN/bin/llvm-ar CC=$host_cc CXX=$host_cxx LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -ldflags "-X main.VERSION=$(date -u +%Y%m%d) -s -w" -o v2ray-plugin
 file v2ray-plugin
 $PLATFORM-readelf -d v2ray-plugin
 cd ..
 
 git clone --depth 1 https://github.com/xtaci/kcptun.git
 cd kcptun/client
-env AR=$TOOLCHAIN/bin/llvm-ar CC=${PLATFORM}${API}-clang CXX=${PLATFORM}${API}-clang++ LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -mod=vendor -ldflags "-X main.VERSION=$(date -u +%Y%m%d) -s -w" -o kcptun-client
+env AR=$TOOLCHAIN/bin/llvm-ar CC=$host_cc CXX=$host_cxx LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -mod=vendor -ldflags "-X main.VERSION=$(date -u +%Y%m%d) -s -w" -o kcptun-client
 cd ${CI_PROJECT_DIR:?}
 
 git clone --depth 1 https://github.com/XIU2/CloudflareSpeedTest
 cd CloudflareSpeedTest
 go get -d ./...
-env AR=$TOOLCHAIN/bin/llvm-ar CC=${PLATFORM}${API}-clang CXX=${PLATFORM}${API}-clang++ LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -ldflags "-X main.VERSION=$(date -u +%Y%m%d) -s -w"
+env AR=$TOOLCHAIN/bin/llvm-ar CC=$host_cc CXX=$host_cxx LD=$TOOLCHAIN/bin/ld GO111MODULE=on CGO_ENABLED=1 GOOS=android GOARCH=$go_arch go build -ldflags "-X main.VERSION=$(date -u +%Y%m%d) -s -w"
 cd ..
-
 
 wget http://dist.schmorp.de/libev/libev-4.33.tar.gz
 tar zxvf libev-4.33.tar.gz
@@ -64,7 +63,7 @@ rm -rf libev-4.33.tar.gz
 cd libev-4.33
 chmod +x autogen.sh
 ./autogen.sh
-env CC=${PLATFORM}${API}-clang CXX=${PLATFORM}${API}-clang++ LD=$TOOLCHAIN/bin/ld ./configure --host=${PLATFORM} --prefix=$NDK_PREFIX
+env CC=$host_cc CXX=$host_cxx LD=$TOOLCHAIN/bin/ld ./configure --host=${PLATFORM} --prefix=$NDK_PREFIX
 make
 make install
 cd ..
@@ -73,7 +72,7 @@ cd simple-obfs
 git submodule update --init
 ./autogen.sh
 #https://www.cnblogs.com/z16166/p/13192665.html
-env CC=${PLATFORM}${API}-clang CXX=${PLATFORM}${API}-clang++ LD=$TOOLCHAIN/bin/ld ./configure --host=${PLATFORM} --disable-documentation
+env CC=$host_cc CXX=$host_cxx LD=$TOOLCHAIN/bin/ld ./configure --host=${PLATFORM} --disable-documentation
 find ./ -name "Makefile" -type f -exec sed -i 's/-lev/-l:libev.a/g' {} +
 make
 $PLATFORM-strip src/obfs-local
@@ -93,7 +92,7 @@ cd ..
 git clone --depth 1 https://github.com/pymumu/smartdns
 patch -p0 <${CI_PROJECT_DIR:?}/patch/smartdns.patch
 cd smartdns
-make CC=${PLATFORM}${API}-clang CXX=${PLATFORM}${API}-clang++ LD=$TOOLCHAIN/bin/ld
+make CC=$host_cc CXX=$host_cxx LD=$TOOLCHAIN/bin/ld
 $PLATFORM-strip src/smartdns
 $PLATFORM-readelf -d src/smartdns
 
@@ -113,20 +112,16 @@ cargo update --manifest-path crates/shadowsocks-tools/Cargo.toml
 #touch /tmp/keepalive
 #bash jobs/debug.sh
 export AR=$TOOLCHAIN/bin/llvm-ar
-export CC=$TOOLCHAIN/bin/${PLATFORM}${API}-clang
+export CC=$host_cc
 export AS=$CC
-export CXX=$TOOLCHAIN/bin/${PLATFORM}${API}-clang++
+export CXX=$host_cxx
 export LD=$TOOLCHAIN/bin/ld
 export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
 export STRIP=$TOOLCHAIN/bin/llvm-strip
-if [[ "$PLATFORM" == "arm-"* ]]; then
-  export CC=$TOOLCHAIN/bin/armv7a-linux-androideabi${API}-clang
-  export CXX=$TOOLCHAIN/bin/armv7a-linux-androideabi${API}-clang++
-fi
-if [[ "$PLATFORM" == "aarch64-"* ]]; then
-  env RUSTFLAGS="-C linker=$CC" cargo build --target "$PLATFORM" --release --features "local-tun local-dns local-tunnel aead-cipher-2022 armv8 neon"
+if [[ $PLATFORM == "aarch64-"* ]]; then
+	env RUSTFLAGS="-C linker=$CC" cargo build --target "$PLATFORM" --release --features "local-tun local-dns local-tunnel aead-cipher-2022 armv8 neon"
 else
-  env RUSTFLAGS="-C linker=$CC" cargo build --target "$PLATFORM" --release --features "local-tun local-dns local-tunnel aead-cipher-2022"
+	env RUSTFLAGS="-C linker=$CC" cargo build --target "$PLATFORM" --release --features "local-tun local-dns local-tunnel aead-cipher-2022"
 fi
 file target/$PLATFORM/release/sslocal
 $PLATFORM-readelf -d target/$PLATFORM/release/sslocal
@@ -140,4 +135,3 @@ $(curl -s -L https://bgp.space/china6.html | grep -oP '([0-9a-fA-F]{0,4}:){1,7}[
 EOF
 #upx --lzma --color -f -v target/$PLATFORM/release/sslocal
 #$PLATFORM-strip target/$PLATFORM/release/sslocal
-
